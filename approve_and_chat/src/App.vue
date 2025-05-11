26580d5b52d78d2ea188a3c4b955bc33005be3bb
viayuu <1761278446@qq.com>
2025-05-11 14:26:44 +0800

    完善界面，最终实现

--- approve_and_chat/src/App.vue
+++ approve_and_chat/src/App.vue
@@ -1,33 +1,10 @@
 <template>
   <div class="container">
-    <header class="my-4">
-      <h1 class="text-center mb-4">Teedy增强功能平台</h1>
-      
-      <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Teedy扩展</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <router-link to="/" class="nav-link" active-class="active">注册申请</router-link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
     
     <main>
       <router-view />
     </main>
     
-    <footer class="mt-5 pt-4 text-center text-muted">
-      <hr>
-      <p>&copy; 2023 Teedy文档管理系统扩展功能</p>
-    </footer>
   </div>
 </template>
 
